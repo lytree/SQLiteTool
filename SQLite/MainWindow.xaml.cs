009737ea8b6acf812d78c6b955dc5401db385ab5
PrideYang <hi.yangzh@outlook.com>
2022-11-11 15:59:53 +0800

    删减不必要代码

--- SQLite/MainWindow.xaml.cs
+++ SQLite/MainWindow.xaml.cs
@@ -18,7 +18,12 @@ namespace SQLite
         {
             InitializeComponent();
 
-
+            Task.Factory.StartNew(() => Thread.Sleep(2500)).ContinueWith(t =>
+            {
+                //note you can use the message queue from any thread, but just for the demo here we 
+                //need to get the message queue from the snackbar, so need to be on the dispatcher
+                MainSnackbar.MessageQueue?.Enqueue("Welcome to Material Design In XAML Toolkit");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
             DataContext = new MainWindowViewModel(MainSnackbar.MessageQueue!);
         }
 
